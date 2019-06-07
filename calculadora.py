d50ed5d179040938d5139314334dabca611249bd
Armando <jaimes087.uz@gmail.com>
2019-06-07 18:24:48 -0500

    Agregado la división fixes #1

--- calculadora.py
+++ calculadora.py
@@ -7,10 +7,13 @@ def mostrarMenu():
 	print('1. Sumar')
 	print('2. Restar')
 	print('3. Multiplicar')
+	print('4. Dividir')
+
 
 sumar = lambda numero1, numero2: numero1 + numero2
 restar = lambda numero1, numero2: numero1 - numero2
 multiplicar = lambda numero1, numero2: numero1 * numero2
+dividir = lambda numero1, numero2: numero1 / numero2
 
 
 # Ejecución del programa
@@ -18,7 +21,7 @@ mostrarMenu()
 
 opcion = input('Opción: ')
 # Si no elige opción válida
-while (opcion != '1') and (opcion != '2') and (opcion != '3'): 
+while (opcion != '1') and (opcion != '2') and (opcion != '3') and (opcion != '4'): 
 	opcion = input('Opción: ')
 	
 numero1 = float(input('Número 1: '))
@@ -28,5 +31,7 @@ if opcion == '1':
 	print(f'{numero1} + {numero2} = {sumar(numero1, numero2)}')
 elif opcion == '2':
 	print(f'{numero1} - {numero2} = {restar(numero1, numero2)}')
-else:
+elif opcion == '3':
 	print(f'{numero1} * {numero2} = {multiplicar(numero1, numero2)}')
+else:
+	print(f'{numero1} / {numero2} = {dividir(numero1, numero2)}')
